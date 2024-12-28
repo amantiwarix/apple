8b9b83b8aebf591813e91fc3bd52ea0cc1e9ff8d
Aman Tiwari <amantiwariadg@gmail.com>
2024-12-28 15:55:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+import './style.css'
+import { Environment, OrbitControls, ScrollControls } from '@react-three/drei'
+import MacContainer from './MacContainer'
+
+
+const App = () => {
+  return (
+    <div className="w-full h-screen font['Helvetica_Now_Display']">
+    <div className="navbar absolute top-0 left-1/2 -translate-x-1/2 flex gap-10 line pt-10 pb-3 ">
+      {['iPhones', "iPad", 'AirPods', 'Mac','iPhones', "iPad", 'AirPods', 'Mac', 'iPhones', "iPad"].map(e => (
+        <a href='' className='text-white font-[500] text-md'>{e}</a>
+      ))}
+    </div>
+    <div className="absolute flex flex-col items-center text-white top-32 left-1/2  -translate-x-1/2 ">
+      <h3 className='masked text-7xl tracking-tighter font-[600]'>Macbook Pro</h3>
+      <h5 className=''>Oh so pro !</h5>
+      <p className='text-center w-3/4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque vero harum facilis magnam ipsam neque? Sit quis!</p>
+    </div>
+    <Canvas camera={{fov:12, position: [0,-10, 220]}}>
+     <Environment files={["https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr"]} />
+     <ScrollControls pages={3}>
+        <MacContainer />
+     </ScrollControls> 
+    </Canvas>
+    </div>
+  )
+}
+
+export default App
